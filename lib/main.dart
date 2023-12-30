69be322be3baed6e75e2df2d5d8434a29441be98
henry <wspoutsource@pixelfield.cz>
2023-12-30 11:08:29 +0700

    add edit and save local

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_flutter/todo.dart';
 
 void main() => runApp(const MyApp());
@@ -26,29 +29,55 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  final List<Todo> _todos = [
-    Todo(
-      title: 'Hello world',
-      dateCreated: DateTime.now(),
-    ),
-    Todo(
-      title: 'Hello world 2',
-      dateCreated: DateTime.now().subtract(
-        const Duration(days: 1),
-      ),
-    ),
-  ];
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      isLoading = true;
+    });
+    SharedPreferences.getInstance().then((value) {
+      _sharedPreferences = value;
+      _getLocalData();
+      isLoading = false;
+    });
+  }
+
+  void _getLocalData() async {
+    final stringData = _sharedPreferences.getString('todo_list') ?? '[]';
+    final json = jsonDecode(stringData) as List;
+    setState(() {
+      _todos = json
+          .map(
+            (e) => Todo.fromJson(e as Map<String, dynamic>),
+          )
+          .toList();
+    });
+  }
+
+  void _saveLocalData() {
+    final json = _todos.map((e) => e.toJson()).toList();
+    final String stringData = jsonEncode(json);
+    _sharedPreferences.setString('todo_list', stringData);
+  }
+
+  late final List<Todo> _todos;
+
+  bool isLoading = false;
+
+  late final SharedPreferences _sharedPreferences;
 
   void _addTodo(Todo newTodo) {
     setState(() {
       _todos.add(newTodo);
     });
+    _saveLocalData();
   }
 
   void _removeTodo(String id) {
     setState(() {
       _todos.removeWhere((element) => element.id == id);
     });
+    _saveLocalData();
   }
 
   void _updateTodo(Todo newTodo) {
@@ -64,6 +93,7 @@ class HomePageState extends State<HomePage> {
       }
       _todos[index] = newTodo;
     });
+    _saveLocalData();
   }
 
   @override
@@ -95,51 +125,84 @@ class HomePageState extends State<HomePage> {
           )
         ],
       ),
-      body: _todos.isEmpty
+      body: isLoading
           ? const Center(
-              child: Text('No todo to show'),
+              child: CircularProgressIndicator(),
             )
-          : ListView.builder(
-              itemCount: _todos.length,
-              itemBuilder: (context, index) {
-                final todo = _todos[index];
-                return ListTile(
-                  title: Text(todo.title),
-                  subtitle: Text(todo.createdDateString),
-                  leading: Checkbox(
-                    value: todo.isCompleted,
-                    onChanged: (value) {
-                      todo.dateCompleted =
-                          todo.isCompleted ? null : DateTime.now();
-                      _updateTodo(todo);
-                    },
-                  ),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.delete),
-                    onPressed: () => _removeTodo(todo.id),
-                  ),
-                );
-              },
-            ),
+          : _todos.isEmpty
+              ? const Center(
+                  child: Text('No todo to show'),
+                )
+              : ListView.builder(
+                  itemCount: _todos.length,
+                  itemBuilder: (context, index) {
+                    final todo = _todos[index];
+                    return ListTile(
+                      title: Text(todo.title),
+                      subtitle: Text(todo.createdDateString),
+                      leading: Checkbox(
+                        value: todo.isCompleted,
+                        onChanged: (value) {
+                          todo.dateCompleted =
+                              todo.isCompleted ? null : DateTime.now();
+                          _updateTodo(todo);
+                        },
+                      ),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () => _removeTodo(todo.id),
+                      ),
+                      onTap: () {
+                        Navigator.of(context)
+                            .push<Todo>(
+                          MaterialPageRoute<Todo>(
+                            builder: (context) => AddTodoPage(
+                              todo: todo,
+                            ),
+                          ),
+                        )
+                            .then(
+                          (value) {
+                            if (value != null) {
+                              _updateTodo(value);
+                            }
+                          },
+                        );
+                      },
+                    );
+                  },
+                ),
     );
   }
 }
 
 class AddTodoPage extends StatefulWidget {
-  const AddTodoPage({super.key});
+  const AddTodoPage({
+    super.key,
+    this.todo,
+  });
+
+  final Todo? todo;
 
   @override
   State<AddTodoPage> createState() => _AddTodoPageState();
 }
 
 class _AddTodoPageState extends State<AddTodoPage> {
-  String _title = '';
+  @override
+  void initState() {
+    _title = widget.todo != null ? widget.todo!.title : '';
+    super.initState();
+  }
+
+  late String _title;
 
   @override
   Widget build(BuildContext context) {
+    final isEdit = widget.todo != null;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Add todo'),
+        title: Text(isEdit ? 'Edit todo' : 'Add todo'),
       ),
       body: ListView(
         padding: const EdgeInsets.all(16),
@@ -163,7 +226,7 @@ class _AddTodoPageState extends State<AddTodoPage> {
           ),
           ElevatedButton(
             onPressed: _title.isEmpty ? null : _onAddTodo,
-            child: const Text('Add'),
+            child: Text(isEdit ? 'Update' : 'Add'),
           ),
         ],
       ),
@@ -177,6 +240,12 @@ class _AddTodoPageState extends State<AddTodoPage> {
   }
 
   void _onAddTodo() {
+    if (widget.todo != null) {
+      final newTodo = widget.todo!;
+      newTodo.title = _title;
+      Navigator.of(context).pop(newTodo);
+      return;
+    }
     final newTodo = Todo(
       title: _title,
     );
--- lib/todo.dart
+++ lib/todo.dart
@@ -10,7 +10,8 @@ class Todo {
     required this.title,
     DateTime? dateCreated,
     this.dateCompleted,
-  })  : id = const Uuid().v4(),
+    String? id,
+  })  : id = id ?? const Uuid().v4(),
         dateCreated = dateCreated ?? DateTime.now();
 
   bool get isCompleted => dateCompleted != null;
@@ -21,4 +22,19 @@ class Todo {
   @override
   String toString() =>
       '${super.toString()}: id: $id, title: $title, dateCreated: $dateCreated, dateCompleted: $dateCompleted';
+
+  factory Todo.fromJson(Map<String, dynamic> json) => Todo(
+        title: json['title'] as String,
+        dateCreated: DateTime.parse(json['dateCreated'] as String),
+        dateCompleted:
+            DateTime.tryParse((json['dateCompleted'] as String?) ?? ''),
+        id: json['id'] as String,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'dateCreated': dateCreated.toIso8601String(),
+        'dateCompleted': dateCompleted?.toIso8601String(),
+        'title': title,
+      };
 }
